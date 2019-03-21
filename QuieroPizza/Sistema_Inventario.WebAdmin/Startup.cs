0a5d2b84c4ce2f7928da4c1398f503fa4ec67bc8
VictorReyes <VictorReyes@DESKTOP-HRQ9S63>
2019-03-20 18:12:27 -0600

    crear poyecto

---
+++ QuieroPizza/Sistema_Inventario.WebAdmin/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Sistema_Inventario.WebAdmin.Startup))]
+namespace Sistema_Inventario.WebAdmin
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
